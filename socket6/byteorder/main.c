1e41c86143d3a75c97aa7204cb772703363d29b1
liuyakun <happy_lucky52@163.com>
2017-03-16 20:18:18 +0800

    byteorder

--- socket6/byteorder/main.c
+++ socket6/byteorder/main.c
@@ -16,6 +16,17 @@ int main (int argc, char *argv[])
 	p = (unsigned char*) &d;
 	printf("%0x %0x %0x %0x\n", p[0], p[1], p[2], p[3]);
 
+	unsigned short a1 = 0x1234;
+	unsigned char *q = (unsigned char*)&a1;
+	printf ("%0x %0x\n", q[0], q[1]);
+
+	unsigned short b1 = htons (a1);
+	q = (unsigned char*)&b1;
+	printf ("%0x %0x\n", q[0], q[1]);
+
+	unsigned short c1 = ntohs (b1);
+	q = (unsigned char*)&c1;
+	printf ("%0x %0x\n", q[0], q[1]);
 
 	return 0;
 }

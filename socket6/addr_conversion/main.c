fdcd67a0c55d2c961e17280602a543aeff77f9de
liuyakun <happy_lucky52@163.com>
2017-03-16 20:34:58 +0800

    addr_conversion

--- socket6/addr_conversion/main.c
+++ socket6/addr_conversion/main.c
@@ -6,12 +6,17 @@ int main (int argc, char *argv[])
 	const char addr[] = "192.168.1.100";
 	unsigned int a = inet_addr (addr);
 
-	printf ("%u\n", a);
+	printf ("%u\n", ntohl(a));
 
 	struct in_addr Saddr;
 	Saddr.s_addr = a;
 
 	printf ("%s\n", inet_ntoa (Saddr));
+
+	struct in_addr Saddr1;
+
+	int ret = inet_aton (addr, &Saddr1);
+	printf ("%u %d\n", ntohl(Saddr1.s_addr), ret);
 
 
 	return 0;

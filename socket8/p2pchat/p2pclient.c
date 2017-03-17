5caf3860143d57b058c5b11eb103f5ff9be04f10
liuyakun <happy_lucky52@163.com>
2017-03-18 00:53:34 +0800

    p2pchat

--- socket8/p2pchat/p2pclient.c
+++ socket8/p2pchat/p2pclient.c
@@ -7,6 +7,7 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <string.h>
+#include <signal.h>
 
 
 #define  ERR_EXIT(m)\
@@ -15,8 +16,7 @@ do {\
 	exit (EXIT_FAILURE);\
 } while (0)
 
-void do_read (int n);
-void do_write (int n);
+void handler ();
 
 const char conn_addr[] = "127.0.0.1";
 
@@ -29,21 +29,61 @@ int main (int argc, char *argv[])
 
 	struct sockaddr_in connect_addr;
 	connect_addr.sin_family = AF_INET;
-	connect_addr.sin_addr.s_addr = htonl (inet_addr (conn_addr));
+	connect_addr.sin_addr.s_addr = inet_addr (conn_addr);
 	connect_addr.sin_port = htons (8888);
 
 	if (-1 == connect (sock, (struct sockaddr*)&connect_addr, sizeof (connect_addr)))
 		ERR_EXIT ("connect");
 
+	pid_t pid;
+	pid = fork ();
+	if (-1 == pid)
+		ERR_EXIT("fork");
 
+	if (0 == pid)//子进程
+	{
+		char receive_buf[1024] = {0};
+		while (1)
+		{
+			memset (receive_buf, 0, sizeof (receive_buf));
+			int ret = read (sock, receive_buf, sizeof (receive_buf));
+			if (-1 == ret)
+				ERR_EXIT ("read");
+			if (0 == ret)
+			{
+				puts ("peer close\n");
+				break;
+			}
+
+			fputs (receive_buf, stdout);
+		}
+		puts ("child close");
+		close (sock);
+		kill (getppid (), SIGUSR1);
+		exit (EXIT_SUCCESS);
+
+	}
+	else
+	{
+		signal (SIGUSR1, handler);
+		char send_buf[1024] = {0};
+		while (fgets (send_buf, sizeof (send_buf), stdin) != NULL)
+		{
+			write (sock, send_buf, strlen (send_buf));		
+			memset (send_buf, 0, sizeof (send_buf));
+		}
+		puts ("parent close");
+		close (sock);
+		exit (EXIT_SUCCESS);
+	}
+
+	close (sock);
+
+	return 0;
 }
 
-void do_read (int n)
+void handler ()
 {
-
-}
-
-void do_write (int n)
-{
-
+	puts ("receive a signal");
+	exit (EXIT_SUCCESS);
 }
--- socket8/p2pchat/p2pserver.c
+++ socket8/p2pchat/p2pserver.c
@@ -1,90 +1,98 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <netinet/in>
+#include <netinet/in.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <string.h>
 #include <errno.h>
+#include <signal.h>
 
-#define ERR_EXIT(m)\
-do {\
+#define ERR_EXIT(m) \
+do { \
 	perror (m); \
-	exit (EXIT_FALURE);\
+	exit (EXIT_FAILURE); \
 } while (0)
 
-void do_read (int n);
-void do_write (int n);
+void handler ();
 
-const char[] host_addr = "127.0.0.1";
+const char *host_addr = "127.0.0.1";
 
 int main (int argc, char *argv[])
 {
 	int listenfd = socket (PF_INET, SOCK_STREAM, 0);
 
 	if (-1 == listenfd)
-		ERR_EXIT ("socket");
+		ERR_EXIT("socket");
 
 	struct sockaddr_in server_addr;
 	server_addr.sin_family = AF_INET;	
-	server_addr.sin_addr.s_addr = htonl (inet_addr (host_addr));
+	server_addr.sin_addr.s_addr = inet_addr (host_addr);
 	server_addr.sin_port = htons (8888);
 	
 	int on = 1;
 	if (-1 == setsockopt (listenfd, SOL_SOCKET,SO_REUSEADDR, &on, sizeof (on)))
-			ERR_EXIT ("setsockopt");
+			ERR_EXIT("setsockopt");
 
 	if (-1 == bind (listenfd, (struct sockaddr*)&server_addr, sizeof (server_addr)))
-		ERR_EXIT ("bind");
+		ERR_EXIT("bind");
 
 	if (-1 == listen (listenfd, SOMAXCONN))
-		ERR_EXIT ("listen");
+		ERR_EXIT("listen");
 
 	struct sockaddr_in peer_addr;
 	socklen_t peer_addr_len = sizeof (peer_addr);
 	int conn = accept (listenfd, (struct sockaddr*)&peer_addr, &peer_addr_len);
 	if (-1 == conn)
-		ERR_EXIT ("accept");
+		ERR_EXIT("accept");
 	printf ("ip: %s, port: %u\n", inet_ntoa (peer_addr.sin_addr), ntohs (peer_addr.sin_port));
 
 	pid_t pid;
 	pid = fork ();
 	if (-1 == pid)
-		ERR_EXIT ("fork");
+		ERR_EXIT("fork");
 	else if (0 == pid)//子进程
 	{
-		do_read (conn);
+		signal (SIGUSR1, handler);
+		char send_buf[1024] = {0};
+		while (fgets (send_buf, sizeof (send_buf), stdin) != NULL)
+		{
+			write (conn, send_buf, strlen (send_buf));
+			memset (send_buf, 0, sizeof (send_buf));
+		}
+
+		exit (EXIT_SUCCESS);
 	}
 	else//父进程
 	{
-		do_write (conn);	
-	}
-}
-
-void do_read (int n)
-{
-	while (1)
-	{
-		char receive_buf[1024];
-		memset (receive_buf, 0, sizeof (receive_buf));
-		int ret = read (n, receive_buf, sizeof (receive_buf));
-		if (-1 == ret)
-			ERR_EXIT ("read");
-		if (0 == ret)
+		while (1)
 		{
-			puts ("peer close\n");
-			break;
+			char receive_buf[1024];
+			memset (receive_buf, 0, sizeof (receive_buf));
+			int ret = read (conn, receive_buf, sizeof (receive_buf));
+			if (-1 == ret)
+				ERR_EXIT("read");
+			if (0 == ret)
+			{
+				puts ("peer close\n");
+				break;
+			}	
+			fputs (receive_buf, stdout);	
 		}	
-		puts (receive_buf);	
+		
+		kill (pid, SIGUSR1);
+		exit (EXIT_SUCCESS);
 	}
+
+	close (conn);
+	close (listenfd);
+	return 0;
 }
 
-void do_write (int n)
+
+void handler ()
 {
-	char send_buf[1024] = {0};
-	while (fgets (send_buf, sizeof (send_buf), stdin) != NULL)
-	{
-		write (n, send_buf, strlen (send_buf));
-	}
+	puts ("receive a signal");
+	exit (EXIT_SUCCESS);
 }
